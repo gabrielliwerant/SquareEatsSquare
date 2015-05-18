6f5f45268727b46e17947e949ca02f804515c96e
Gabriel Liwerant <gabriel.liwerant@gmail.com>
2015-05-18 10:33:38 -0400

    Update build process

--- myapp/Gruntfile.js
+++ myapp/Gruntfile.js
@@ -4,21 +4,48 @@ module.exports = function(grunt) {
         uglify: {
             my_target: {
                 files: {
-                    'build/myapp-min.js': ['build/myapp-concat.js']
+                    'public/build/myapp-min.js': ['public/build/myapp-concat.js']
+                }
+            }
+        },
+
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'public/build/myapp-concat-styles.min.css': ['public/build/myapp-concat-styles.css']
                 }
             }
         },
 
         concat: {
             dist: {
-                src: ['public/javascripts/**/*.js'],
-                dest: 'build/myapp-concat.js'
+                src: [
+                    'public/javascripts/bootstrap.js',
+                    'public/javascripts/constants.js',
+                    'public/javascripts/game-board.js',
+                    'public/javascripts/score.js',
+                    'public/javascripts/ship.js',
+                    'public/javascripts/enemy.js',
+                    'public/javascripts/game-loop.js',
+                    'public/javascripts/game.js'],
+                dest: 'public/build/myapp-concat.js'
+            }
+        },
+
+        concat_css: {
+            all: {
+                src: ['public/stylesheets/**/*.css'],
+                dest: 'public/build/myapp-concat-styles.css'
             }
         },
 
         jshint: {
             beforeconcat: ['public/javascripts/**/*.js'],
-            afterconcat: ['build/myapp-concat.js']
+            afterconcat: ['public/build/myapp-concat.js']
         },
 
         nodemon: {
@@ -31,8 +58,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-concat-css');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-nodemon');
 
-    grunt.registerTask('dev', ['jshint:beforeconcat', 'concat', 'jshint:afterconcat', 'uglify']);
+    grunt.registerTask('dev', ['jshint:beforeconcat', 'concat', 'jshint:afterconcat', 'uglify', 'concat_css', 'cssmin']);
 
 };
